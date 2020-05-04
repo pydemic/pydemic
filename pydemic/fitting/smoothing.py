0cbf1370526587e152bd88762f39565b767ecfd5
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2020-05-04 15:09:41 -0300

    Initial fitting support

---
+++ pydemic/fitting/smoothing.py
@@ -0,0 +1,64 @@
+from typing import Union
+
+import numpy as np
+import statsmodels.api as sm
+
+from ..types import Result
+
+
+def smoothed_diff(
+    data: np.ndarray,
+    *,
+    bias: float = 1e-4,
+    damped: bool = True,
+    linear: bool = False,
+    fast_init: bool = False,
+    retall: bool = False,
+    **kwargs,
+) -> Union[np.ndarray, Result]:
+    """
+    Load a series of values and use Holt's algorithm to smooth the finite
+    differences.
+
+    Args:
+        data:
+            Input data.
+        damped:
+            If True (default), enable damping of the exponential trend.
+        linear:
+            If True, make the trend linear instead of exponential.
+        bias:
+            Holt's algorithm require strictly positive values (i.e., greater than,
+            but not equal to zero). This parameter controls a bias term that is
+            added to the baseline to prevent the existence of null values.
+            This transformation is not used if linear=True.
+        fast_init:
+            If True, prevents using the slower brute force search during
+            initialization of the model.
+        retall:
+            If True, return a named tuple (value, info) with both the computed
+            smoothed array and the statistical model used to calibrate the
+            parameters.
+        **kwargs:
+
+    Returns:
+        Either a :cls:`np.ndarray` of smoothed differences or a :cls:`result` value.
+    """
+    data = np.diff(data, prepend=0.0)
+
+    # Guarantee that there are no zero values in the
+    # dataset
+    if not linear:
+        data = np.maximum(data, 0.0)
+        S = data.sum()
+        data *= 1 - bias
+        data += bias * S
+
+    # Choose empirically validated values
+    kwargs.setdefault("smoothing_level", 1 / 5)
+
+    # Holt exponential smoothing
+    holt = sm.tsa.Holt(data, exponential=not linear, damped=damped)
+    res = holt.fit(use_brute=not fast_init, **kwargs)
+    out = res.fittedvalues
+    return Result(out, res) if retall else out
---
+++ pydemic/fitting/time_dependent_growth.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+
+from .smoothing import smoothed_diff
+
+
+def time_dependent_K(cases: pd.Series, method="smoothing", **kwargs):
+    """
+    Compute a time-dependent exponential growth factor from the given times
+    series of cases, fatalities or some strongly correlated proxy.
+
+    Args:
+        cases:
+            Time-series of cases.
+
+    Returns:
+        A DataFrame with ["value", "low", "high"] columns. The  main inference
+        is in the "value" interval. If the chosen method does not compute
+        confidence intervals, low and high will contain only NaNs.
+    """
+    compute = TimeDependentK(cases)
+    return compute(method, **kwargs)
+
+
+#
+# Class-based facade to smoothing methods
+#
+class TimeDependentK:
+    """
+    A class that implements inference methods for time-dependent K as instance
+    methods in the class.
+    """
+
+    def __init__(self, cases):
+        self.cases = cases
+        self.dates = cases.index
+        self.data = cases.values
+
+    def __call__(self, method, **kwargs):
+        fn = getattr(self, "method_" + method)
+        out = fn(**kwargs)
+        return pd.Series(out, index=self.dates)
+
+    def method_smoothing(self, **kwargs):
+        res = naive_holt_smoothing(self.data, **kwargs)
+        return res.fittedvalues
+
+
+#
+# Private implementations of smoothing methods.
+# Those methods do not pre-process input or post-process outputs and generally
+# work with raw numpy arrays.
+#
+def naive_holt_smoothing(ys, max_daily_growth=2, **kwargs):
+    """
+    A double-smoothing method.
+    """
+
+    max_factor = np.log(max_daily_growth)
+    deltas = smoothed_diff(ys, **kwargs)
+    dirty_ks = np.minimum(np.diff(np.log(deltas), prepend=0), max_factor)
+
+    smoothing_level = kwargs.setdefault("smoothing_level", 0.1)
+    holt = sm.tsa.Holt(dirty_ks, damped=True)
+    return holt.fit(smoothing_level=smoothing_level)
