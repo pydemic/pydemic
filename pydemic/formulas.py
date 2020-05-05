Fix incorrect growth rate formula

pydemic/formulas.py
@@ -2,9 +2,9 @@ from numbers import Real
 
 import numpy as np
 
-from ..params import select_param, ParamLike
+from .params import select_param, ParamLike
 
-FUNCTIONS = {"R0_from_K": {}, "K": {}}
+FUNCTIONS = {"R0_from_K": {}, "K": {}, "R0": {}}
 FUNCTIONS_R0_FROM_K = FUNCTIONS["R0_from_K"]
 FUNCTIONS_K = FUNCTIONS["K"]
 LN_2 = np.log(2)
@@ -114,7 +114,7 @@ def doubling_time(model, params=None, **kwargs) -> Real:
 # SIR formulas
 #
 @register()
-def R0_SIR(params: ParamLike = None, k=None, gamma=None, **kwargs) -> float:
+def R0_from_K_SIR(params: ParamLike = None, K=None, gamma=None, **kwargs) -> float:
     """
     Return R0 from the exponential growth factor K and the other model
     parameters.
@@ -123,13 +123,13 @@ def R0_SIR(params: ParamLike = None, k=None, gamma=None, **kwargs) -> float:
     as attributes or methods.
     """
     gamma = select_param("gamma", params, gamma)
-    k = select_param("K", params, k)
+    K = select_param("K", params, K)
 
-    return 1.0 + k / gamma
+    return 1.0 + K / gamma
 
 
 @register()
-def K_SIR(params: ParamLike = None, r0=None, gamma=None, **kwargs) -> float:
+def K_SIR(params: ParamLike = None, R0=None, gamma=None, **kwargs) -> float:
     """
     Return the exponential growth factor K from R0 and the other model
     parameters.
@@ -138,9 +138,9 @@ def K_SIR(params: ParamLike = None, r0=None, gamma=None, **kwargs) -> float:
     as attributes or methods.
     """
     gamma = select_param("gamma", params, gamma)
-    r0 = select_param("R0", params, r0)
+    R0 = select_param("R0", params, R0)
 
-    return gamma * (r0 - 1)
+    return gamma * (R0 - 1)
 
 
 #
@@ -148,30 +148,23 @@ def K_SIR(params: ParamLike = None, r0=None, gamma=None, **kwargs) -> float:
 #
 @register()
 @register("R0_from_K", "SEAIR")
-def R0_from_K_SEIR(params: ParamLike = None, k=None, gamma=None, sigma=None, **kwargs) -> float:
+def R0_from_K_SEIR(params: ParamLike = None, K=None, gamma=None, sigma=None, **kwargs) -> float:
     gamma = select_param("gamma", params, gamma)
     sigma = select_param("sigma", params, sigma)
-    k = select_param("K", params, k)
+    K = select_param("K", params, K)
 
-    return 1.0 + (gamma + sigma + k) * k / (gamma * sigma)
+    return 1.0 + (gamma + sigma + K) * K / (gamma * sigma)
 
 
 @register()
 @register("K", "SEAIR")
-def K_SEIR(params: ParamLike = None, r0=None, gamma=None, sigma=None, **kwargs) -> float:
-    """
-    Return the exponential growth factor K from R0 and the other model
-    parameters.
-
-    Params can be anything that has the epidemiological parameters
-    as attributes or methods.
-    """
+def K_SEIR(params: ParamLike = None, R0=None, gamma=None, sigma=None, **kwargs) -> float:
     gamma = select_param("gamma", params, gamma)
     sigma = select_param("sigma", params, sigma)
-    r0 = select_param("R0", params, r0)
-    S = sigma + gamma
+    R0 = select_param("R0", params, R0)
+    mu = sigma + gamma
 
-    return 0.5 * S * (np.sqrt(1 + 4 * (r0 - 1) * sigma * gamma / (S * S)) - 1)
+    return 0.5 * mu * (np.sqrt(1 + 4 * (R0 - 1) * sigma * gamma / (mu * mu)) - 1)
 
 
 #

pydemic/models/abstract_seir.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from .abstract_sir import AbstractSIR as Base
 from .. import params
+from ..formulas import K, R0_from_K
 from ..utils import state_property, param_property
 
 
@@ -20,10 +21,11 @@ class AbstractSEIR(Base):
     # Derived expressions
     @property
     def K(self):
-        g = self.gamma
-        s = self.sigma
-        R0 = self.R0
-        return 0.5 * (s + g) * (np.sqrt(1 + 4 * (R0 - 1)) - 1)
+        return K("SEIR", gamma=self.gamma, sigma=self.sigma, R0=self.R0)
+
+    @K.setter
+    def K(self, value):
+        self.R0 = R0_from_K("SEIR", gamma=self.gamma, sigma=self.sigma, K=value)
 
     # Simulation state
     susceptible = state_property(0)