4775b3dcb002c44525dfb1d13a9d46301075b662
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2020-05-13 19:48:55 -0300

    Clean types and modules

--- pydemic/region.py
+++ pydemic/region.py
@@ -1,1 +1,7 @@
 from mundi_demography import age_distribution, age_pyramid, population
+from mundi_healthcare import (
+    hospital_capacity,
+    hospital_capacity_public,
+    icu_capacity,
+    icu_capacity_public,
+)
--- pydemic/types.py
+++ pydemic/types.py
@@ -1,3 +1,9 @@
 from collections import namedtuple
 
 Result = namedtuple("Result", ["value", "info"])
+
+
+class ImproperlyConfigured(Exception):
+    """
+    Exception raised when trying to initialize object with improper configuration.
+    """
