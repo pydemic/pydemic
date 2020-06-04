3b0b9e5f55853f33a2af2d18f5e9b95d641b7c1a
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2020-06-04 14:46:14 -0300

    Support composite regions

---
+++ pydemic/region/multi_region.py
@@ -0,0 +1,150 @@
+from typing import Optional, FrozenSet
+
+import pandas as pd
+import sidekick as sk
+
+from mundi import Region, region
+from .pydemic_property import PydemicProperty
+from .pyplot_property import PyplotProperty
+from ..diseases import disease as get_disease
+from ..properties.decorators import cached
+
+AGGREGATE_METHOD = {
+    "population": "sum",
+    "age_distribution": "sum",
+    "age_pyramid": "sum",
+    "icu_capacity": "sum",
+    "hospital_capacity": "sum",
+    "icu_capacity_public": "sum",
+    "hospital_capacity_public": "sum",
+}
+
+
+class CompositePydemicProperty(PydemicProperty):
+    region: "CompositeRegion"
+
+    @cached(ttl="epidemic_curve")
+    def epidemic_curve(self, disease=None, **kwargs) -> pd.DataFrame:
+        fn = get_disease(disease).epidemic_curve
+        curves = (fn(r, **kwargs) for r in self.region.regions)
+        return self.region.aggregate_values(curves, "sum")
+
+
+class CompositePyplotProperty(PyplotProperty):
+    pass
+
+
+class CompositeRegion(Region):
+    """
+    An aggregate of regions.
+    """
+
+    name: str
+    regions: FrozenSet[Region]
+
+    pydemic = property(CompositePydemicProperty)
+    plot = property(CompositePyplotProperty)
+
+    def __new__(cls, sub_regions, **kwargs):
+        sub_regions = frozenset(map(region, sub_regions))
+        if not sub_regions:
+            raise ValueError("cannot start with empty list of regions")
+        new = object.__new__(cls)
+        new.__dict__["id"] = id
+        new.__dict__["regions"] = sub_regions
+        new.__dict__.update(**kwargs)
+        return new
+
+    def __hash__(self):
+        return hash((self.id, self.regions))
+
+    def __getstate__(self):
+        return self.id, self.regions
+
+    def __setstate__(self, state):
+        id_, regions = state
+        self.__dict__["id"] = id_
+        self.__dict__["regions"] = regions
+
+    def __str__(self):
+        regions = [r.id for r in self.regions]
+        if len(regions) > 5:
+            regions = regions[:5] + "..."
+        regions = ", ".join(regions)
+
+        return (
+            f"Composite Region\n"
+            f"id       : {self.id}\n"
+            f"name     : {self.name}\n"
+            f"type     : {self.type}\n"
+            f"subtype  : {self.subtype}\n"
+            f"regions  : {regions}"
+        )
+
+    def _get_field(self, key):
+        method = AGGREGATE_METHOD.get(key, "single")
+        return self.aggregate(key, method)
+
+    def aggregate(self, field, by):
+        """
+        Aggregate values with function.
+        """
+        values = (region._get_field(field) for region in self.regions)
+        return self.aggregate_values(values, by)
+
+    def aggregate_values(self, values, by):
+        """
+        Given a sequence or iterable with one value per region, aggregate
+        values using the given method.
+        """
+        if by == "sum":
+            value, values = sk.uncons(values)
+            try:
+                value = value.copy()
+            except AttributeError:
+                return sum(values, value)
+            else:
+                for v in values:
+                    value += v
+                return value
+        elif by == "sum-pop":
+            populations = (region.population for region in self.regions)
+            values = (x * pop for x, pop in zip(values, populations))
+            return self.aggregate_values(values, "sum")
+        elif by == "mean":
+            return self.aggregate(values, "sum") / len(self.regions)
+        elif by == "mean-pop":
+            return self.aggregate(values, "sum-pop") / self.population
+        elif by == "single":
+            value, *error = set(values)
+            if error:
+                raise ValueError("regions do not share the same value")
+            return value
+        else:
+            raise ValueError(f"invalid aggregation method: {by}")
+
+    #
+    # Hierarchies
+    #
+    @property
+    def parent(self) -> Optional["Region"]:
+        """
+        Common parent to all regions.
+        """
+        parent, *error = set(region.parent for region in self.regions)
+        if error:
+            raise ValueError("regions do not share a common parent.")
+        return parent
+
+    def children(self, dataframe=False, deep=False, which="both"):
+        """
+        Return list of children.
+        """
+        if dataframe:
+            raise NotImplementedError
+
+        children = {*self.regions}
+        if deep:
+            for region in self.regions:
+                children.update(region.children(deep=True, which=which))
+        return list(children)
--- pydemic/region/pydemic_property.py
+++ pydemic/region/pydemic_property.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
 from .base import RegionProperty
+from .. import fitting as fit
 from ..diseases import disease as get_disease, DiseaseParams
+from ..properties.decorators import cached
+from ..types import ValueStd
 
 
 class PydemicProperty(RegionProperty):
@@ -11,6 +14,7 @@ class PydemicProperty(RegionProperty):
 
     __slots__ = ()
 
+    @cached(ttl="epidemic_curve")
     def epidemic_curve(self, disease=None, **kwargs) -> pd.DataFrame:
         """
         Return epidemic curves for region.
@@ -27,3 +31,50 @@ class PydemicProperty(RegionProperty):
         """
         disease = get_disease(disease)
         return disease.params(region=self.region, **kwargs)
+
+    @cached(ttl="epidemic_curve")
+    def estimate_Rt(self, model, disease=None, **kwargs) -> pd.DataFrame:
+        """
+        Compute R(t) from the epidemic curves. This is function is just a
+        shortcut to :func:`fit.estimate_Kt` that automatically binds to the
+        current region.
+        """
+        return self._estimate_R(fit.estimate_Rt, model, disease, **kwargs)
+
+    @cached(ttl="epidemic_curve")
+    def estimate_R0(self, model, disease=None, **kwargs) -> ValueStd:
+        """
+        Compute R0 from the epidemic curves. This is function is just a
+        shortcut to :func:`fit.estimate_Kt` that automatically binds to the
+        current region.
+        """
+        return self._estimate_R(fit.estimate_R0, model, disease, **kwargs)
+
+    def _estimate_R(self, func, model, disease, **kwargs):
+        disease = get_disease(disease)
+        curves = self.epidemic_curve(disease)
+        if isinstance(model, str) and "params" not in kwargs:
+            kwargs["params"] = disease.params(region=self.region)
+        return func(model, curves, **kwargs)
+
+    @cached(ttl="epidemic_curve")
+    def estimate_Kt(self, disease=None, **kwargs) -> pd.DataFrame:
+        """
+        Compute K(t) from the epidemic curves. This is function is just a
+        shortcut to :func:`fit.estimate_Kt` that automatically binds to the
+        current region.
+        """
+        return self._estimate_K(fit.estimate_Kt, disease, **kwargs)
+
+    @cached(ttl="epidemic_curve")
+    def estimate_K(self, disease=None, **kwargs) -> ValueStd:
+        """
+        Compute K from the epidemic curves. This is function is just a
+        shortcut to :func:`fit.estimate_K` that automatically binds to the
+        current region.
+        """
+        return self._estimate_K(fit.estimate_K, disease, **kwargs)
+
+    def _estimate_K(self, func, disease, **kwargs):
+        curves = self.epidemic_curve(disease)
+        return func(curves, **kwargs)
